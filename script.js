0ee70574328db3d5bb85a97a842d6f2e4572f809
MouadNaji <moua0005@stud.kea.dk>
2020-11-19 14:01:15 +0100

    header disc/mobile styling done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("JS");
+let mobileMenubutton = document.querySelector("#mobile-menu-button");
+let mobileMenuClosebutton = document.querySelector("#close-menu-button");
+
+mobileMenubutton.addEventListener("click", openMobileMenu);
+mobileMenuClosebutton.addEventListener("click", closeMobileMenu);
+
+function openMobileMenu() {
+  document.querySelector("#mobile-menu").style.display = "grid";
+}
+
+function closeMobileMenu() {
+  document.querySelector("#mobile-menu").style.display = "none";
+}
